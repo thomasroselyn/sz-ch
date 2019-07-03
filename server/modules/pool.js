857e96c4a8ab8daa2830990091a469c958bc74a6
Thomas Roselyn <tgroselyn@gmail.com>
2019-07-03 08:23:02 -0500

    updated readme and pool

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -3,7 +3,7 @@ const pg = require('pg');
 const Pool = pg.Pool;
 
 const pool = new Pool({
-    database: 'sezzle_challenge',
+    database: process.env.DATABASE_URL || 'sezzle_challenge',
     host: 'localhost',
     port: 5432,
     max: 10,
