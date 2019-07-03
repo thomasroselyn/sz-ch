7d51a1b0c9a58b9703a97bb76ec48d25f7c587e8
Thomas Roselyn <tgroselyn@gmail.com>
2019-07-03 08:51:30 -0500

    updated pool

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -4,7 +4,7 @@ const Pool = pg.Pool;
 
 const pool = new Pool({
     database: process.env.DATABASE_URL || 'sezzle_challenge',
-    host: 'ec2 - 23 - 21 - 109 - 177.compute- 1.amazonaws.com' || 'localhost',
+    host: 'ec2-23-21-109-177.compute-1.amazonaws.com' || 'localhost',
     port: process.env.PGPORT || 5432,
     max: 10,
     idleTimeoutMillis: 10000
